3090214fbf4031a76e7e8f5682e73e4189b4b816
Anik Sengupta <98419733+anikredngold@users.noreply.github.com>
2025-12-17 13:16:39 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,19 +3,9 @@ package hello;
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
+    public static final byte[] DUMMY_DATA = { /* 100,000,000+ random bytes here */ };
+
     public static void main(String[] args) {
-        String message = "Hello World! ";
-        StringBuilder hugeContent = new StringBuilder();
-        for (int i = 0; i < 7000000; i++) { // Adjust the count if needed for exact size
-            hugeContent.append(message);
-        }
-        final String veryLargeString = hugeContent.toString();
-
-        System.out.println(veryLargeString.substring(0, 13) + "...");
-     // LocalTime currentTime = new LocalTime();
-	//	  System.out.println("The current local time is: " + currentTime);
-
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+        System.out.println("Hello World");
     }
 }
