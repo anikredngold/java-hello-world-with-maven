3fb642ad99b29b71ad6e87717f8c5a4dab70dc38
Anik Sengupta <98419733+anikredngold@users.noreply.github.com>
2025-12-17 13:10:12 +0530

    Update HelloWorld.java
    added

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -4,11 +4,7 @@ import org.joda.time.LocalTime;
 
 public class HelloWorld {
     public static void main(String[] args) {
-
-String message = "Hello World! ";
-        // Repeat the message enough times to bloat the file.
-        // A 100MB uncompressed file is the target for compression testing.
-        // This loop will generate a very large string literal during compilation.
+        String message = "Hello World! ";
         StringBuilder hugeContent = new StringBuilder();
         for (int i = 0; i < 7000000; i++) { // Adjust the count if needed for exact size
             hugeContent.append(message);
@@ -16,6 +12,9 @@ String message = "Hello World! ";
         final String veryLargeString = hugeContent.toString();
 
         System.out.println(veryLargeString.substring(0, 13) + "...");
+     // LocalTime currentTime = new LocalTime();
+	//	  System.out.println("The current local time is: " + currentTime);
+
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
     }
